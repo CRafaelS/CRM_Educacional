d70f2a20c8aaa7e59cf5fe82bd1272153155e4be
CRafaelS <carlos.rafael@engenharia.ufjf.br>
2022-12-10 02:17:08 -0300

    feat(model): Curso

---
+++ src/Models/Curso.cs
@@ -0,0 +1,12 @@
+namespace educacional.Models;
+
+public class Curso
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public string CargaHoraria { get; set; }
+    public string Preco { get; set; }
+    public string Categoria { get; set; }
+    public ICollection<Matricula> Matriculas { get; set; }
+}
