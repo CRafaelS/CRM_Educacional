2786ad2a7e3fc8217a8fc24e1ae20305f6f8bade
CRafaelS <carlos.rafael@engenharia.ufjf.br>
2022-12-11 16:39:59 -0300

    feat(CursoController):

---
+++ src/Controllers/CursosController.cs
@@ -0,0 +1,26 @@
+using educacional.Models;
+using educacional.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace educacional.Controllers
+{
+    [ApiController]
+    [Route("cursos")]
+
+    public class CursosController : Controller
+    {
+        private readonly CursoRepository _cursoRepository;
+
+        public CursosController(CursoRepository cursoRepository)
+        {
+            _cursoRepository = cursoRepository;
+        }
+
+        [HttpPost]
+        public IActionResult CriarCurso([FromBody] Curso curso)
+        {
+            return Created("", _cursoRepository.criarCurso(curso));
+        }
+    }
+    
+}
