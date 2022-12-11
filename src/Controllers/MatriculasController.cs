aa18242181e86be7a5a18c31d536d47ca6be9f91
CRafaelS <carlos.rafael@engenharia.ufjf.br>
2022-12-11 16:40:39 -0300

    feat(MatriculaController): criando Matricula controller com metodo post

---
+++ src/Controllers/MatriculasController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using educacional.Repository;
+using educacional.Models;
+
+namespace educacional.Controllers
+{
+    [ApiController]
+    [Route("matriculas")]
+    public class MatriculasController : Controller
+    {
+        private readonly MatriculaRepository _matriculaRepository;
+
+        public MatriculasController(MatriculaRepository matriculaRepository)
+        {
+            _matriculaRepository = matriculaRepository;
+        }
+
+        [HttpPost]
+        public IActionResult CriarMatricula([FromBody] MatriculaDTO matricula)
+        {
+            return Created("", _matriculaRepository.criarMatricula(matricula));
+        }
+    }
+}
